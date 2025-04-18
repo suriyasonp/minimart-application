e92f2bf44e073af08390d14b3e1e11fb5f3a134e
Suriya S <suriya.sonphu@liebherr.com>
2025-04-18 16:48:04 +0700

    Add Services folder and enhance project documentation
    - Added a "Services" folder reference in `MinimartApp.Application.csproj`.
    - Introduced a nullable `Description` property in the `Product` class.
    - Updated `MinimartApp.sln` to include a "Solution Items" section with `.gitignore` and `README.md`.
    - Added `ExtensibilityGlobals` section with `SolutionGuid` in the solution file.
    - Revised `README.md` for improved project overview, tech stack details, and entity relationships.

--- MinimartApp.Domain/Entities/Product.cs
+++ MinimartApp.Domain/Entities/Product.cs
@@ -7,6 +7,7 @@ namespace MinimartApp.Domain.Entities
         [Key]
         public Guid Id { get; set; }
         public string Name { get; set; } = null!;
+        public string? Description { get; set; }
         public decimal Price { get; set; } = 0.0m;
         public string Category { get; set; } = null!;
     }
